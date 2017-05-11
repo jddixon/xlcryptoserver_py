93b9f226e441159f8a85a3c3971b23495a50d353
Jim Dixon <jddixon@gmail.com>
2017-05-11 11:22:53 -0700

    drop dist/* from repos; plus minor edits

--- xlcryptoserver/__init__.py
+++ xlcryptoserver/__init__.py
@@ -2,8 +2,8 @@
 
 """ Crypto library for python XLattice packages. """
 
-__version__ = '0.0.2'
-__version_date__ = '2017-03-27'
+__version__ = '0.0.3'
+__version_date__ = '2017-05-11'
 
 __all__ = ['__version__', '__version_date__',
            'XLCryptoServerError', ]
